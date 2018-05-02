d6c2fda965eb39c683532ef8b3d0977029837df5
Alex <icemosco@gmail.com>
2018-05-01 23:11:53 -0500

    cambios en ruta e imagen
    change

--- admin/views/acreditados.php
+++ admin/views/acreditados.php
@@ -3,6 +3,7 @@
 
 <div class="wrapper_right">
 	PROGRAMAS ACREDITADOS
+	<img src="../img/programas.png" />
 </div><!--main_wrapper-->
 <script src="../js/jquery-3.3.1.min.js"></script>
 <script src="../js/main.js"></script>
