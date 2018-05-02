ab521aee63b00231ce12ac7e9d4238f8d7d3c836
perezosa <perezosa790@gmail.com>
2018-05-01 21:22:42 -0500

    Se agrega login a la pagina y algunos scripts nuevos

--- admin/php/config.php
+++ admin/php/config.php
@@ -1,8 +1,12 @@
 <?php
-  // Variables para la conexion con la BD 
-  $dbHost             = "107.180.4.49";
-  $dbUser             = "conaed_admin";
-  $dbPassword         = "caludia0Peniche2018";
-  $dbName             = "cms-conaed";
+    //============================
+	//  DATOS DE CONEXION A LA BD 
+	//============================
+	
+    define('DB_HOST','107.180.4.49');
+    define('DB_USER','conaed_admin');
+    define('DB_PASS','caludia0Peniche2018'); 
+    define('DB_NAME','cms-conaed');
+    define('DB_CHARSET','UTF8');
 
 ?>
