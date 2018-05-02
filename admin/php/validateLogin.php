9817a9f6432986faf033e389a205784a17e9aaf6
perezosa <perezosa790@gmail.com>
2018-05-01 21:22:28 -0500

    Se agrega login a la pagina y algunos scripts nuevos

--- admin/php/validateLogin.php
+++ admin/php/validateLogin.php
@@ -1,10 +1,32 @@
 <?php
-	include("php/DbConnect.php"); 
-    include("php/config.php");
+	include("../php/config.php");
+	include("../php/DbConnect.php");
+	include("../php/Usuario.php"); 
     
-    $usr = $_POST['usuario'];
-    $pwd = $_POST['contrasena'];
+    $usr = trim($_POST['usuario']);
+    $pwd = trim($_POST['contrasena']);
     
     
+    if(empty( $usr )){
+	    $enlace = "../views/index.php?msg=Ingrese un usuario";
+		header("location: {$enlace}");
+		exit;    
+    }
     
+    if(empty( $pwd )){
+	    $enlace = "../views/index.php?msg=Ingrese una contraseña";
+		header("location: {$enlace}");
+		exit;
+    }
+    
+    $oUsr  = new Usuario();
+	  
+	$oUsr->setUsuario($usr);
+	$oUsr->setPassword( md5($pwd) );
+	  
+	$err   = $oUsr->login($_POST);
+
+	if(!empty( $err )){
+		header("location: ../views/login.php?msg=".$err); 
+	}
 ?>	
