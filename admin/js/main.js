3257e8b6f37d5be2089c66b70b820338da23b7ba
perezosa <perezosa790@gmail.com>
2018-05-04 15:59:46 -0500

    algunas validaciones de campos

--- admin/js/main.js
+++ admin/js/main.js
@@ -37,21 +37,35 @@ $(document).ready(function(){
 				return false;
 			}
 	
-		$('.slider').append('<div class="slider_fill"><span class="title_sl">Slider '+nom_slide+'</span><div class="left mr"><span class="indications">Imágen .jpg ó .png</span><input type="text" name="num_slider[]" id="num_slider"  size="2" style="width: 5%"/><ul><li></li></ul><div class="img_loaded"></div><div class="path">ruta del archivo</div><div class="cont_r"><input type="file" name="imagenSlider[]" id="imagenSlider[]"class="file_upload" name="file" style="width: 125px;height: 35px;bottom: 28%;right: 0%;" /><a href="javascript:void(0)" class="btn_cargar">Cargar</a></div></div><!--left--><div class="right"><div <span class="">Titulo (100 caracteres máx)</span><span class="conteo_char">0 caracteres</span></div><textarea name="titulo[]" id="titulo" id="subtitulo" class="" required ></textarea><div class="sub_text_cont"><span class="">Subtitulo (225 caracteres máx)</span><span class="conteo_char">0 caracteres</span></div><textarea name="subtitulo[]" class="" required ></textarea></div><input type="hidden" name="idSlide[]" id="idSlide"  value=""/></div><!--slider_fill-->');
+		$('.slider').append('<div class="slider_fill"><span class="title_sl">Slider '+nom_slide+'</span><div class="left mr"><span class="indications">Imágen .jpg ó .png</span><input type="text" name="num_slider[]" id="num_slider"  value="" size="2" style="width: 5%"/><ul><li></li></ul><div class="img_loaded"><img src="../../img/slider/img_1.png"></div><div class="path">ruta del archivo</div><div class="cont_r"><input type="file" name="imagenSlider[]" id="imagenSlider[]"class="file_upload" name="file" style="width: 125px;height: 35px;bottom: 28%;right: 0%;" /><a href="javascript:void(0)" class="btn_cargar">Cargar</a></div></div><!--left--><div class="right"><div class="sub_text_cont"><span class="">Titulo (100 caracteres máx)</span><span class="conteo_char">0 caracteres</span><textarea name="titulo[]" id="titulo" id="titulo" class="infoSlide" maxlength="100" required ></textarea></div><div class="sub_text_cont"><span class="">Subtitulo (225 caracteres máx)</span><span class="conteo_char">0 caracteres</span><textarea name="subtitulo[]" id="subtitulo" class="infoSlide"  maxlength="225" required ></textarea></div><input type="hidden" name="idSlide[]" id="idSlide"  value=""/></div><!--slider_fill-->');
 	
 	});
 	
 	//Coloca el numero de caracteres de los textarea de la seccion de slide
-	$('.infoSlide').keyup(function( e ){
+	$(document).on('keyup','.infoSlide',function (e) {	
 		var contador  = e.currentTarget.parentElement.children[1];
 		var numCarac  = $(this).val().length;
 		$(contador).html( numCarac + " caracteres");
 
     });
+
+    //Limitamos a que sean solo numeros 
+   	$(document).on('keypress','#num_slider',function (e) {	
+		var key = window.event ? e.which : e.keyCode;
+  		if (key < 48 || key > 57) {
+    		e.preventDefault();
+  		}
+    }); 
+
+    //Validamos que el numero solo sea de 1-5
+	$(document).on('keyup','#num_slider',function (e) {		
+  		if(e.currentTarget.value > 5 || e.currentTarget.value < 1){
+  			console.log(e.currentTarget.value);
+  			e.currentTarget.value = '';
+  		}
+
+    });
      
-
-
-
 
 });//ready
 
