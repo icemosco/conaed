a585ff018342eab2cb34909d06c5599e9d27fbe3
perezosa <perezosa790@gmail.com>
2018-05-11 03:02:25 -0500

    No message

--- admin/js/main.js
+++ admin/js/main.js
@@ -57,22 +57,10 @@ $('.muestra_msg').click(function(){
 
 })
 
-
-<<<<<<< HEAD
-=======
 $('.edit').click(function(){
 	$(".new_programa").css("display", "none");
 	$(this).parent().next().find( ".new_programa" ).css( "display", "block" );
 });
-/*  
-	/*$('.file_upload').change(function(){
-	     var fupload=$(this).val();
-	      $(this).parent().parent().find('.path').html(fupload);
-	      $(this).parent().parent().find('.img_loaded').html('<img src="'+fupload+'" />');
-	     
-	 });*/
-	 
->>>>>>> 7405362fec6800ea24fa1274a16fc2d18aa894ca
 	 
 	
 	$(document).on('change','.file_upload',function (e) {
@@ -81,8 +69,6 @@ $('.edit').click(function(){
 		$(this).parent().parent().find('.path').html(fupload);
 		$(this).parent().parent().find('.img_loaded').html('<img src="'+tmppath+'" />');
 	});
-xs
-
 	
 	var nom_slide  = 2;
 	
