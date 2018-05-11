922efffa4f2df6a6f37544c29c94cf2001431423
Alex <icemosco@gmail.com>
2018-05-10 22:38:27 -0500

    date picker
    date picker

--- admin/views/home.php
+++ admin/views/home.php
@@ -21,7 +21,8 @@
 	<div class="header_int">
 		<h2 class="muestra_msg">Slider Home Page</h2>
 		<div class="search">
-			
+			<!--<a href="javascript:void(0)" class="add_n_slider" title="Agregar un nuevo slider">
+				<img src="../img/plus.png" alt="" /></a>-->
 		</div>
 	</div>
 	<div class="forms_cont sliders">
@@ -38,14 +39,10 @@
 		<form class="prog" name="programas" id="" action="./home.php" method="post" enctype="multipart/form-data">
 			<style>
 			.new_programa{width:100%; float:left;overflow:hidden;}
-			.new_programa ul{width:100%; height:auto;display:block;}
+			.new_programa ul{width:100%; height:auto;display:block;margin-bottom:5%;}
 			.new_programa ul li{width:48%; display: inline-block;padding:5px;outline:1px solid red;}
 			</style>
-			
-			
-			<button type="submit"  class="save_btn" name="guardar" id="guardar" >Guardar</button>
-		</form>
-		<div class="new_programa">
+			<div class="new_programa">
 				<ul>
 					<li>
 						<label>Universidad o Institucion Educativa:</label>
@@ -70,46 +67,9 @@
 			</div>
 			
 			
-		
-	</div><!--forms_cont-->
-	<div class="forms_cont evaluadores">
-		
-			
-		<form class="prog" name="programas" id="" action="./home.php" method="post" enctype="multipart/form-data">
-			<style>
-			.new_programa{width:100%; float:left;overflow:hidden;}
-			.new_programa ul{width:100%; height:auto;display:block;}
-			.new_programa ul li{width:48%; display: inline-block;padding:5px;outline:1px solid red;}
-			</style>
-			
 			
 			<button type="submit"  class="save_btn" name="guardar" id="guardar" >Guardar</button>
 		</form>
-		<div class="new_programa">
-				<ul>
-					<li>
-						<label>Universidad o Institucion Educativa:</label>
-						<input type="text" name="nombre_uni" max-lenght="500" class="" required="required" placeholder="">
-					</li>
-					<li>
-						<label>Página Web:</label>
-						<input type="text" name="pagina_web" max-lenght="500" class="" required="required" placeholder="http:// ó https://">
-					</li>
-				</ul>
-				<ul>
-					<li>
-						<label>Vigencia desde:</label>
-						<input type="text" name="vigencia_ini " max-lenght="100" id="datepickerini" required="required" placeholder="">
-					</li>
-					<li>
-						<label>Vigencia hasta:</label>
-						<input type="text" name="vigencia_fin " max-lenght="100" id="datepickerfin" required="required" placeholder="">
-					</li>
-				</ul>
-
-			</div>
-			
-			
 		
 	</div><!--forms_cont-->
 	
@@ -128,7 +88,7 @@
 	$( function() {
     $( "#datepickerini" ).datepicker();
     $( "#datepickerfin" ).datepicker();
-  } );
+   } );
 </script>
 </body>
 
