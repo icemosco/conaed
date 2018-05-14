69f4e23154957e14f10cd4d3cc6acfcdbfe6670b
Alex <icemosco@gmail.com>
2018-05-14 08:39:19 -0500

    cambios imagen heaedr
    imagen header

---
+++ admin/php/views/acreditados.php
@@ -0,0 +1,16 @@
+<?php include_once('./header.php') ?>
+
+
+<div class="wrapper_right">
+	PROGRAMAS ACREDITADOS
+	<img src="../img/programas.png" />
+</div><!--main_wrapper-->
+<script src="../js/jquery-3.3.1.min.js"></script>
+<script src="../js/main.js"></script>
+<script>
+
+
+</script>
+</body>
+
+</html>
---
+++ admin/php/views/evaluadores.php
@@ -0,0 +1,15 @@
+<?php include_once('./header.php') ?>
+
+
+<div class="wrapper_right">
+	EVALUADORES
+</div><!--main_wrapper-->
+<script src="../js/jquery-3.3.1.min.js"></script>
+<script src="../js/main.js"></script>
+<script>
+
+
+</script>
+</body>
+
+</html>
