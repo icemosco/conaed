0d8464205cdc9feba7473950584b605447cd10e9
Alex <icemosco@gmail.com>
2018-05-18 01:06:54 -0500

    cambios
    cambios con btn nuevo

--- admin/js/main.js
+++ admin/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	//MENU
 
 	$('.btns_menu').click(function(){
-	
+
 		var btn=$(this).attr('rel');
 		$('.btns_menu').removeClass('selected');
 		$('.icon').removeClass('selected');
@@ -15,32 +15,33 @@ $(document).ready(function(){
 		    case "sliders":
 		        	$('.'+btn).show();
 		        	$('.header_int > h2').html('Slider Home Page');
-		        	$('.header_int > .search').html('<a href="javascript:void(0)" class="add_n_slider" title="Agregar un nuevo slider"><img src="../img/plus.png" alt="" /></a>');
+		        	$('.header_int > .search').html('<a href="javascript:void(0)" class="add_n_slider" title="Agregar un nuevo slider">Nuevo</a>');
 
 		        break;
 		    case "programas":
 		        	$('.'+btn).show();
 		        	$('.header_int > h2').html('Programas Acreditados');
-		        	$('.header_int > .search').html('<a href="javascript:void(0)" rel="programas_plus" class="add_item" title="Agregar una nueva universidad"><img src="../img/plus.png" alt="" /></a>');
+		        	$('.header_int > .search').html('<a href="javascript:void(0)" rel="programas_plus" class="add_item" title="Agregar una nueva universidad">Nuevo</a>');
 		        break;
 		    case "evaluadores":
 		        	$('.'+btn).show();
 		        	$('.header_int > h2').html('Padrón de Evaluadores');
-		        	$('.header_int > .search').html('<a href="javascript:void(0)" rel="evaluadores_plus" class="add_item" title="Agregar un nuevo evaluador"><img src="../img/plus.png" alt="" /></a>');
+		        	$('.header_int > .search').html('<a href="javascript:void(0)" rel="evaluadores_plus" class="add_item" title="Agregar un nuevo evaluador">Nuevo</a>');
 		        break;
 		    case "asociados":
 		        	$('.'+btn).show();
 		        	$('.header_int > h2').html('Referencias y Asociados');
-		        	$('.header_int > .search').html('<a href="javascript:void(0)" rel="asociados_plus" class="add_item" title="Agregar un nuevo Asociado"><img src="../img/plus.png" alt="" /></a>');
+		        	$('.header_int > .search').html('<a href="javascript:void(0)" rel="asociados_plus" class="add_item" title="Agregar un nuevo Asociado">Nuevo</a>');
 		        break;
 		    case "temasynoticias":
 		        	$('.'+btn).show();
 		        	$('.header_int > h2').html('Temas y Noticias');
+		        	$('.header_int > .search').html('<a href="javascript:void(0)" rel="usuarios_plus" class="add_item" title="Agregar un nuevo Usuario">Nuevo</a>');
 		        
 		    case "usuarios":
 		        	$('.'+btn).show();
 		        	$('.header_int > h2').html('Gestor de Usuarios');
-		        	$('.header_int > .search').html('<a href="javascript:void(0)" rel="usuarios_plus" class="add_item" title="Agregar un nuevo Usuario"><img src="../img/plus.png" alt="" /></a>');
+		        	$('.header_int > .search').html('<a href="javascript:void(0)" rel="usuarios_plus" class="add_item" title="Agregar un nuevo Usuario">Nuevo</a>');
 		        break;
 		   
 		}
