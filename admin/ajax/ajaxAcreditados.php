85e0cb78f752282ad2b101c664372ffc31e6adbc
perezosa <perezosa790@gmail.com>
2018-05-18 14:14:09 -0500

    corrigiendo reemplazo

--- admin/ajax/ajaxAcreditados.php
+++ admin/ajax/ajaxAcreditados.php
@@ -4,19 +4,23 @@
 	include_once("../php/DbConnect.php");
 	include_once("../php/Modelo.php");
 	
-	$oMod           = new Modelo();
-	$accion 	    = $_POST['accion'];
-	$idAsociados    = $_POST['idAsociados'];
-	$ordenAsociados	= $_POST['ordenAsociados'];
-
+	$oMod         = new Modelo();
+	$accion 	  = $_POST['accion'];
+	$idAcreditado = $_POST['id'];
+	$nombre		  =  $_POST['nombre']; 
+	$pagina		  =  $_POST['pagina'];
+	$anio		  =  $_POST['anio'];
+	$categoria    =  $_POST['categoria'];
 	
-	function fnOrdenAsociados( $idAsociado, $ordenAsociado )
+	function fnGuardarAcreditados( $nombreUni, $paginaWeb, $anio, $categoria, $idAcreditado = '' )
 	{
 		global $oMod;
-		foreach($idAsociado as $key => $id){
-			$oMod->fnActualizaOrdenAsociado($id, $ordenAsociado[$key]);
-		}
-		return "OK";
+		$error = '';
+		if( empty($idAcreditado )){
+			$id = $oMod->fnInsertaAcreditados( $nombreUni, $paginaWeb, $anio, $categoria );
+		}else{
+			$id = $oMod->fnActualizaAcreditados( $idAcreditado, $nombreUni, $paginaWeb, $anio, $categoria );
+		}	
 	}
 	
 	if($estatus == 1) $estatus = '0';
@@ -27,10 +31,10 @@
 	
 	switch( $accion ){
 		case 'eliminar':
-			//$oMod->fnEliminarAcreditados( $idAcreditado );
+			$oMod->fnEliminarAcreditados( $idAcreditado );
 		break;
-		case 'guardarOrden':
-			fnOrdenAsociados( $idAsociados, $ordenAsociados );
+		case 'guardar':
+			fnGuardarAcreditados( $nombre, $pagina, $anio, $categoria, $idAcreditado );
 		break;
 	}
 	
