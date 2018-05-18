7d1bac261b18ad3c152015f13d250f347d482c49
perezosa <perezosa790@gmail.com>
2018-05-18 14:05:27 -0500

    se agrega template para noticias

--- admin/views/home.php
+++ admin/views/home.php
@@ -5,6 +5,7 @@
 	include_once("../php/ControllerEvaluadores.php");
 	include_once("../php/ControllerUsuarios.php");
 	include_once("../php/ControllerAsociados.php");
+	include_once("../php/ControllerNoticias.php");
 
 	//======================= SLIDER
 	//Guardado / Edicion
@@ -50,11 +51,12 @@
 ?>
 
 <div class="wrapper_right">
+	<!------------------------- -->
+	<!-- -----------SLIDER -->
+	<!------------------------- -->
 	<div class="header_int">
 		<h2 class="muestra_msg">Slider Home Page</h2>
 		<div class="search">
-			<!--<a href="javascript:void(0)" class="add_n_slider" title="Agregar un nuevo slider">
-				<img src="../img/plus.png" alt="" /></a>-->
 		</div>
 	</div>
 	<div class="forms_cont sliders">
@@ -68,7 +70,9 @@
 
 
 
-
+	<!------------------------- -->
+	<!-- --------ACREDITADOS -->
+	<!------------------------- -->
 	<div class="forms_cont programas">
 		<div class="add_programa oxygenlight"><? echo fnTemplateNuevoAcreditado(); ?></div>
 		<form class="prog" name="acreditados" id="acreditados" action="./home.php" method="post">
@@ -80,7 +84,9 @@
 
 
 
-
+	<!------------------------- -->
+	<!-- -------EVALUADORES   -->
+	<!------------------------- -->
 	<div class="forms_cont evaluadores">
 		<div class="add_evaluador"><?php echo fnTemplateNuevoEvaluadores(); ?></div>
 		<form class="prog" name="evaluadores" id="evaluadores" action="./home.php" method="post" >
@@ -92,7 +98,9 @@
 
 
 
-
+	<!------------------------- -->
+	<!-- -----------USUARIO -->
+	<!------------------------- -->
 	<div class="forms_cont usuarios oxygenlight">
 		<div class="add_usuario"><?php echo fnTemplateNuevoUsuario(); ?></div><!--add_usuario-->
 		<form class="users_gest" name="usuarios" id="usuarios" action="./home.php" method="post">
@@ -101,78 +109,30 @@
 		<ul class="paginator"> <?php echo $infoUsus['paginador']; ?> </ul>
 	</div><!--forms_cont-->
 
+
+
+	<!------------------------- -->
+	<!-- --------ASOCIADOS -->
+	<!------------------------- -->
 	<div class="forms_cont asociados oxygenlight">
 		<div class="add_asociado"><?php echo fnTemplateNuevoAsociado(); ?></div><!--add_asociado-->
-
-		
 		<form class="frm_asociados" name="asociados" id="asociados" action="./home.php" method="post" enctype="multipart/form-data">
 			<div class="reel_asociados">
-				<ul>
-
-					<?php echo fnTemplateAsociados(); ?>
-
-				</ul>
+				<ul><?php echo fnTemplateAsociados(); ?></ul>
 			</div>
-			<button type="button" class="save_btn" id="guardar_asociados_i" onclick="">Guardar</button>
+			<button type="button" class="save_btn" id="guardar_asociados_i" onclick="guardarOrdenAsociados()">Guardar</button>
 		</form>
 	</div><!--form_cont-asociados-->
 	
 
+	<!------------------------- -->
+	<!-- --------NOTICIAS -->
+	<!------------------------- -->
 	<div class="forms_cont temasynoticias oxygenlight">
-		<div class="add_tema" style="display:none;">
-			<div class="temas_fill">
-				<div class="left mr">
-					<span class="indications">Imágen .jpg ó .png</span>
-					<div class="img_loaded" style="margin-top:0;" ><img src="../../img/noticias/"/></div>
-					<div class="path">ruta del archivo</div>
-					<div class="cont_r">
-						<input type="hidden" name="" value=""/>
-						<input type="file" name="" class="file_upload_2" name="file" />
-						<a href="javascript:void(0)" class="btn_cargar">Cargar</a>
-					</div><!--cont_r-->
-				</div><!--left-->
-				
-				<div class="right">
-					<div class="sub_text_cont">
-						<textarea name="titulo_tema" class="infoSlide requeridoSlider"  maxlength="200" placeholder="Titulo del Tema" ></textarea>
-					</div>
-					
-					<div class="sub_text_cont">
-						<textarea name="contenido_tema" class="infoSlide requeridoSlider" maxlength="" placeholder="Nota completa del Tema"></textarea>
-					</div>
-					
-				</div>
-				<input type="hidden" name="" value=""/> 	
-			</div><!--tema_fill-->
-		</div>
 		<!---termina add tema-->
 		<form class="frm_temasynoticias" name="temasynoticias" id="temasynoticias" action="./home.php" method="post" enctype="multipart/form-data">
-			<div class="temas_fill">
-				<div class="left mr">
-					<span class="indications">Imágen .jpg ó .png</span>
-					<div class="img_loaded" style="margin-top:0;" ><img src="../../img/noticias/"/></div>
-					<div class="path">ruta del archivo</div>
-					<div class="cont_r">
-						<input type="hidden" name="" value=""/>
-						<input type="file" name="" class="file_upload_2" name="file" />
-						<a href="javascript:void(0)" class="btn_cargar">Cargar</a>
-					</div><!--cont_r-->
-				</div><!--left-->
-				
-				<div class="right">
-					<div class="sub_text_cont">
-						<textarea name="titulo_tema" class="infoSlide requeridoSlider"  maxlength="200" placeholder="Titulo del Tema" ></textarea>
-					</div>
-					
-					<div class="sub_text_cont">
-						<textarea name="contenido_tema" class="infoSlide requeridoSlider" maxlength="" placeholder="Nota completa del Tema"></textarea>
-					</div>
-					
-					
-				</div><!--right-->
-				<input type="hidden" name="" value=""/> 	
-			</div><!--tema_fill-->
-
+			<?php echo fnTemplateNoticias(); ?>
+			<button type="submit"  class="save_btn" name="guardar_noticia" id="guardar_noticia" >Guardar</button>
 		</form>
 	</div>
 	
@@ -199,19 +159,18 @@
 	 
 	 $(document).ready(function(){
 		 
-		function showMessages( msg ){
+		 //Muestra mensajes del slider
+		 var msgSlider = "<?php echo $mostrarMsgSlider; ?>";
+		 if(msgSlider ){
+			 var msg = "<span>"+msgSlider+"</span>";
+			 
 			$(".msg").html(msg); 
 		    $('.msg').fadeIn(200).animate({"bottom":"0px"}, "slow");
 			setTimeout(function() {
 				$(".msg").fadeOut(1500).animate({"bottom":"-50px"}, "slow");
     		},3000);
-		 } 
-		 
-		 //Muestra mensajes del slider
-		 var msgSlider = "<?php echo $mostrarMsgSlider; ?>";
-		 if(msgSlider ){
-			 var msg = "<span>"+msgSlider+"</span>";
-			 showMessages( msg );
+
+
 		 } 
 	 })	 
  </script>
